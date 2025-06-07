f775156049937c2259f6ed0d586e0d2c648cc49e
Cristobal Chumaceiro <176019782+cristobalchumaceiro@users.noreply.github.com>
2025-06-07 17:20:50 +0100

    Complete problem 06_repeatString
    Submission for problem 06_repeatString, passing all tests.

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(str, multi) {
+    if (multi < 0) return "ERROR"
+    let newStr = ""
+    for (let i = 0; i < multi; i++) {
+        newStr += str
+    }
+    return newStr
 };
 
 // Do not edit below this line
