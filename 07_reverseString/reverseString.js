3818ea47107c89ea7e979277c0e12d666fbb792b
Cristobal Chumaceiro <176019782+cristobalchumaceiro@users.noreply.github.com>
2025-06-07 17:36:42 +0100

    Complete problem 07_reverseString
    Submission for problem 07_reverseString, passing all tests.

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let splitStr = str.split("")
+    let reversedStr = []
+    for (let i = 0; i < splitStr.length; i++) {
+        reversedStr.unshift(splitStr[i])
+    }
+    reversedStr = reversedStr.join("")
+    return reversedStr
 };
 
 // Do not edit below this line
