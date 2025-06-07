f6a320360dc9bc30428921335a6c7ce9b3f4e577
Cristobal Chumaceiro <176019782+cristobalchumaceiro@users.noreply.github.com>
2025-06-07 18:40:03 +0100

    Complete problem 08_removeFromArray
    Submission for problem 08_removeFromArray, passing all tests.

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...removed) {
+    let index
+    for (i = 0; i < removed.length; i++) {
+        while (arr.some((value) => value === removed[i])) {
+            index = arr.findIndex(data => data === removed[i])
+            arr.splice(index, 1)
+        }
+    }
+    return arr
 };
 
 // Do not edit below this line
