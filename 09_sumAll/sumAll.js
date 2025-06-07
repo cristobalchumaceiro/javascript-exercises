2bf45849215790cde412a19f10bd492340c467eb
Cristobal Chumaceiro <176019782+cristobalchumaceiro@users.noreply.github.com>
2025-06-07 19:02:07 +0100

    Complete problem 09_sumAll
    Submission for problem 09_sumAll, passing all tests.

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (a >= 0 && b >= 0 && Number.isInteger(a) && Number.isInteger(b)) {
+        if (a < b) return ((((b - a) + 1) / 2) * (a + b))
+        else return ((((a - b) + 1) / 2) * (b + a)) 
+    }
+    else return "ERROR"
 };
 
 // Do not edit below this line
