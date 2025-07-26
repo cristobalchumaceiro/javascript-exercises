25075f71a852167776496dd35fb544e1853f701b
Cristobal Chumaceiro Rangel <176019782+cristobalchumaceiro@users.noreply.github.com>
2025-07-26 14:46:26 +0100

    Complete problem 14_fibonacci
    Submission for problem 14_fibonacci, passing all tests.

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,26 @@
-const fibonacci = function() {
+const fibonacci = function(x) {
 
+    x = Number.parseInt(x)
+
+    if (x === 0) {
+        return 0
+    }
+    else if (x < 0) {
+        return "OOPS"
+    }
+    
+    const arr = new Array(x)
+
+    if (x >= 2) {
+        arr[0] = 1
+        arr[1] = 1
+    }
+    else arr[0] = 1
+    
+    for (i = 2; i < x; i++) {
+        arr[i] = (arr[(i-1)] + arr[(i-2)])
+    }
+    return arr[(x-1)]
 };
 
 // Do not edit below this line
