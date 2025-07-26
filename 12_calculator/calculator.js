9b2e46d298410dc9dcb3b373460642cb005381b4
Cristobal Chumaceiro Rangel <176019782+cristobalchumaceiro@users.noreply.github.com>
2025-07-26 13:50:00 +0100

    Complete problem 12_calculator
    Submission for problem 12_calculator, passing all tests.

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((prev, current) => prev + current, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((prev, current) => prev * current)
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	return Math.pow(x, y)
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+  let product = 1
+	for (i = x; i > 0; i--) {
+    product *= i
+  }
+  return product
 };
 
 // Do not edit below this line
