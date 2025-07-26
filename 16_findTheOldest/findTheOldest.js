25030765060e0a00953fff6fa698025782bb74d2
Cristobal Chumaceiro Rangel <176019782+cristobalchumaceiro@users.noreply.github.com>
2025-07-26 15:28:38 +0100

    Complete problem 16_findTheOldest
    Submission for problem 16_findTheOldest, passing all tests.

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce((oldest, currentPerson) => {
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath)
+        const currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath)
+        return oldestAge < currentAge ? currentPerson : oldest
+    })
+};
 
+const getAge = function(birthYear, deathYear) {
+    if (!deathYear) {
+        deathYear = new Date().getFullYear();
+    }
+    return deathYear - birthYear
 };
 
 // Do not edit below this line
